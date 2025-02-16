b7c64ed171e23a51d88e0bd99d4a96fa7e8beaf7
Viktor <palchinskij19@gmail.com>
2025-02-16 16:58:14 +0300

    16.02.24 - isLoading isFetching isPanding

--- src/modules/todoList/TodoList.tsx
+++ src/modules/todoList/TodoList.tsx
@@ -5,18 +5,26 @@ import classNames from 'classnames';
 
 export function TodoList() {
     const [page, setPage] = useState(1);
+    const [enabled, setEnable] = useState(false);
     const {
         data: todoItems,
         error,
         isPending,
-        isPlaceholderData
+        isFetching,
+        isLoading,
+        status,
+        fetchStatus,
+        isPlaceholderData,
     } = useQuery({
         queryKey: ['tasks', 'list', { page }],
         queryFn: (meta) => todoListApi.getTodoList({ page }, meta),
         placeholderData: keepPreviousData,
+        enabled,
     });
 
-    if (isPending) {
+    console.log({ status, fetchStatus });
+
+    if (isLoading) {
         return <div>Loading...</div>;
     }
 
@@ -27,12 +35,15 @@ export function TodoList() {
     return (
         <div className="p-5 mx-auto max-w[1200] mt-10">
             <h1 className="text-3xl font-bold underline">TodoList</h1>
+            <button onClick={() => setEnable((e) => !e)}>
+                Toggle enabled
+            </button>
             <div
                 className={classNames('flex flex-col gap-4 mt-5', {
                     'opacity-50': isPlaceholderData,
                 })}
             >
-                {todoItems.data.map(({ id, text }) => (
+                {todoItems?.data.map(({ id, text }) => (
                     <div
                         className="border border-slate-300 rounded p-3"
                         key={id}
@@ -54,7 +65,7 @@ export function TodoList() {
                 <button
                     onClick={() =>
                         setPage((prev) =>
-                            Math.min(prev + 1, todoItems.pages)
+                            Math.min(prev + 1, todoItems?.pages ?? 1)
                         )
                     }
                     className="p-3 rounded border border-teal-500"
