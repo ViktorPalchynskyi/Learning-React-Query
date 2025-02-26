2f7ac014334235ee038c0b72d8620a3ba05c8e8a
Viktor <palchinskij19@gmail.com>
2025-02-26 15:46:58 +0300

    26.02.24 - custom hook

--- src/modules/todoList/TodoList.tsx
+++ src/modules/todoList/TodoList.tsx
@@ -1,29 +1,9 @@
-import { useInfiniteQuery } from '@tanstack/react-query';
-import { todoListApi } from './api';
 import { useCallback, useRef, useState } from 'react';
 import classNames from 'classnames';
+import useTodoList from './useTodoList';
 
 export function TodoList() {
-    const [enabled, setEnable] = useState(false);
-    const {
-        data: todoItems,
-        error,
-        isLoading,
-        status,
-        fetchStatus,
-        isPlaceholderData,
-        fetchNextPage,
-        hasNextPage,
-        isFetchingNextPage,
-    } = useInfiniteQuery({
-        enabled,
-        ...todoListApi.getTodoListInfinityQueryOptions(),
-    });
-    const cursorRef = useIntersection(() => {
-        fetchNextPage();
-    });
-
-    console.log({ status, fetchStatus });
+    const { error, todoItems, isLoading, cursor } = useTodoList();
 
     if (isLoading) {
         return <div>Loading...</div>;
@@ -36,14 +16,7 @@ export function TodoList() {
     return (
         <div className="p-5 mx-auto max-w[1200] mt-10">
             <h1 className="text-3xl font-bold underline">TodoList</h1>
-            <button onClick={() => setEnable((e) => !e)}>
-                Toggle enabled
-            </button>
-            <div
-                className={classNames('flex flex-col gap-4 mt-5', {
-                    'opacity-50': isPlaceholderData,
-                })}
-            >
+            <div className={classNames('flex flex-col gap-4 mt-5')}>
                 {todoItems?.map(({ id, text }) => (
                     <div
                         className="border border-slate-300 rounded p-3"
@@ -53,32 +26,7 @@ export function TodoList() {
                     </div>
                 ))}
             </div>
-
-            <div className="flex gap-2 mt-4" ref={cursorRef}>
-                {!hasNextPage && <div>No data</div>}
-                {isFetchingNextPage && <div>Loading...</div>}
-            </div>
+            {cursor}
         </div>
     );
-}
-
-export function useIntersection(onItersect: () => void) {
-    const unsibscribe = useRef(() => {});
-
-    return useCallback((el: HTMLDivElement | null) => {
-        const observer = new IntersectionObserver((enteries) => {
-            enteries.forEach((intersection) => {
-                if (intersection.isIntersecting) {
-                    onItersect();
-                }
-            });
-        });
-
-        if (el) {
-            observer.observe(el);
-            unsibscribe.current = () => observer.disconnect;
-        } else {
-            unsibscribe.current();
-        }
-    }, []);
 }
---
+++ src/modules/todoList/useTodoList.tsx
@@ -0,0 +1,54 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { todoListApi } from './api';
+import { useCallback, useRef } from 'react';
+
+export function useIntersection(onItersect: () => void) {
+    const unsibscribe = useRef(() => {});
+
+    return useCallback((el: HTMLDivElement | null) => {
+        const observer = new IntersectionObserver((enteries) => {
+            enteries.forEach((intersection) => {
+                if (intersection.isIntersecting) {
+                    onItersect();
+                }
+            });
+        });
+
+        if (el) {
+            observer.observe(el);
+            unsibscribe.current = () => observer.disconnect;
+        } else {
+            unsibscribe.current();
+        }
+    }, []);
+}
+
+export default function useTodoList() {
+    const {
+        data: todoItems,
+        error,
+        isLoading,
+        fetchNextPage,
+        hasNextPage,
+        isFetchingNextPage,
+    } = useInfiniteQuery({
+        ...todoListApi.getTodoListInfinityQueryOptions(),
+    });
+    const cursorRef = useIntersection(() => {
+        fetchNextPage();
+    });
+
+    const cursor = (
+        <div className="flex gap-2 mt-4" ref={cursorRef}>
+            {!hasNextPage && <div>No data</div>}
+            {isFetchingNextPage && <div>Loading...</div>}
+        </div>
+    );
+
+    return {
+        error,
+        todoItems,
+        isLoading,
+        cursor,
+    };
+}
